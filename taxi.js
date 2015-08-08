f94f2f399a6470a2a8fbad7cc44aa85115ff8f13
Lance <Lance@thelpstory.com>
2015-08-08 09:24:35 -0600

    Completed Taxi exercise

--- taxi.js
+++ taxi.js
@@ -1,11 +1,28 @@
 function Taxi(driverName, maxNumberOfPassengers) {
-  // your code here
+  this.driverName = driverName;
+  this.maxNumberOfPassengers = maxNumberOfPassengers;
+  this.passengers = [];
 }
 
 Taxi.prototype.addPassenger = function(passengerName) {
-  // your code here
+  this.passengers.push(passengerName);
 };
 
+Taxi.prototype.passengerCount = function() {
+  return this.passengers.length;
+};
+
+Taxi.prototype.full = function () {
+  if(this.passengerCount() >= this.maxNumberOfPassengers){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Taxi.prototype.dropOffPassengers = function() {
+  this.passengers = [];
+};
 // your code here
 
 // DO NOT MODIFY BELOW THIS COMMENT:
